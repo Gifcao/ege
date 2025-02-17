aa5208c4963924bb33e8c2e4ae2d18f687dbe1aa
VKramarenko <gifagang@gmail.com>
2025-02-17 19:55:25 +0200

    разбор

---
+++ 10-02-25/task 5 - 4585.py
@@ -0,0 +1,12 @@
+ans =[]
+for N in range(1, 10000):
+    R = bin(N)[2:]
+#сумма чисел
+    if R.count('1') % 2 == 0:
+        R = '10' + R[2:] + '0'
+    else:
+        R = '11' + R[2:] + '1'
+    R = int(R,2)
+    if R >=16:
+       ans.append(N)
+print(min(ans))
