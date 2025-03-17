9453945d1bff6f08b1ec90c6c172dfbcb181064f
VKramarenko <gifagang@gmail.com>
2025-03-17 20:02:58 +0200

    тяж

---
+++ 17-03-2025/task 8 - 18484.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+alph = sorted(set('ПАВСИКАКИЙ'))
+cnt = 0
+for i in product(alph,repeat=6):
+    i = ''.join(i)
+    if 'АИ' in i or 'ИА' in i or 'АА' in i or 'ИИ' in i:
+        cnt +=1
+        if i == 'КАКААА':
+            print(cnt)
